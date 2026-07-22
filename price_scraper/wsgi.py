edb8efa10c54b6330c322d5c96049f905232a13a
Dardi2205 <dardi2205@users.noreply.github.com>
2026-07-22 22:46:14 +0200

    Run migrations in wsgi.py startup

--- price_scraper/wsgi.py
+++ price_scraper/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'price_scraper.settings')
+
+call_command('migrate', '--run-syncdb', verbosity=0)
 
 application = get_wsgi_application()
